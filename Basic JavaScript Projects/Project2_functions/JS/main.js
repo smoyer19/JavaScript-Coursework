a8bbdced3a96881128311d4c23192b482707bb17
smoyer19 <smoyer19@gmail.com>
2020-12-31 17:34:30 -0600

    another push
    pushing to get through

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function Assignment() {
+    var str = "something for the assignment.";
+    var result = str.fontcolor("blue");
+    document.getElementById ("Blue_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML =sentence;
+}
