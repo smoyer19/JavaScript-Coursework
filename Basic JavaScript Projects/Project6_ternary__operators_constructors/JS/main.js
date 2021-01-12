c0ab6e78423c6e6910488934e4b495fc67d1f5c7
smoyer19 <smoyer19@gmail.com>
2021-01-11 18:58:53 -0600

    assignment push
    Step 129 JavaScript assignment

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,31 @@
+function Ride_Function() {// Function with two values 
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+// Object constructor example
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep","Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+    document.getElementById("New_and_This").innerHTML = "Emily drives a " + Emily.Vehicle_Color + "-colored" + Emily.Vehicle_Model + " manufactured in " +Emily.Vehicle_Year;
+}
+// Nested Function
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count ();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
